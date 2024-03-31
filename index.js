749ddbeae52e191e6bdbc19be75ef1972c4f74bd
Virginia Gichira <gichiravirginia@gmail.com>
2024-03-31 16:06:32 +0300

    add

--- index.js
+++ index.js
@@ -1,1 +1,27 @@
 // Write your solution in this file!
+var customerName = "bob";
+function upperCaseCustomerName() {
+  customerName = customerName.toUpperCase();
+  return customerName;
+}
+
+//upperCaseCustomerName();
+
+//Function that sets best customer to 'not bob'
+function setBestCustomer() {
+  bestCustomer = "not bob";
+  console.log(bestCustomer);
+}
+//Function overrides the bestcustomer
+function overwriteBestCustomer() {
+  bestCustomer = "maybe bob";
+  console.log(bestCustomer);
+}
+
+// Declare a constant in global scope called leastFavoriteCustomer and assign it some initial value.
+const leastFavoriteCustomer = "somebody";
+
+// Write a function called changeLeastFavoriteCustomer() that attempts to change that constant.
+function changeLeastFavoriteCustomer() {
+  leastFavoriteCustomer = "nobody";
+}
